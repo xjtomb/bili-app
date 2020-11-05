b9af8dc666d05036fde6bf2a76d010734dd6bd1f
xjtomb <xjtomb@qq.com>
2020-11-05 22:34:45 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>第一个VUE工程</h1>
+    <div style="height:70px;">
+    <Item>
+      <h4>我的第一个组件</h4>
+    </Item>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Item from "./components/Item"
+export default {
+  components:{
+    Item
+  }
+}
+</script>
---
+++ src/components/Item.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="item" :class="{action:isActive}">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        isActive:{
+            type:Boolean,  //约事该属性的类型是Boolean
+            //required:true,  //约束该属性是必须传递
+            default:false,  //属性的默认值
+        }
+    },
+}
+</script>
+
+<style scoped>
+.action{
+    background-color: #e7e7e7;
+}
+.item{
+    cursor: pointer;
+    width:100%;
+    height:100%;
+}
+.item:hover{
+    background-color: #f4f4f4;
+}
+</style>
